dfec1fdf2e4881f36b29c909013a1d3670d00cef
Nick Boughton <nicholasboughton@gmail.com>
2021-10-05 15:27:39 +0100

    tweak journal entry

--- src/components/JournalEntry.vue
+++ src/components/JournalEntry.vue
@@ -1,11 +1,13 @@
 <template>
-  <q-expansion-item class="q-px-sm" header-class="q-px-none q-py-none" :default-opened="open" expand-icon-toggle>
-    <template v-slot:header class="row full-width q-gutter-sm">
-      <i-input class="col-grow" label="Title" v-model="campaign.data.journal[index].title" />
-      <q-btn class="col-shrink" flat dense :icon="pinIcon(index)" @click="pin(index)" />
-      <q-btn class="col-shrink" v-if="config.data.edit" flat dense icon="delete" @click="$emit('remove')">
-        <q-tooltip>Delete this journal entry</q-tooltip>
-      </q-btn>
+  <q-expansion-item class="q-px-sm" header-class="q-py-none q-px-none" :default-opened="open" expand-icon-toggle>
+    <template v-slot:header>
+      <div class="row full-width items-center">
+        <i-input class="col-grow q-mr-sm" label="Title" v-model="campaign.data.journal[index].title" />
+        <q-btn class="col-shrink" flat dense :icon="pinIcon(index)" @click="pin(index)" />
+        <q-btn class="col-shrink" v-if="config.data.edit" flat dense icon="delete" @click="$emit('remove')">
+          <q-tooltip>Delete this journal entry</q-tooltip>
+        </q-btn>
+      </div>
     </template>
 
     <q-editor
